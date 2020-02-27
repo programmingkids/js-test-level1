e051451b4aafcdf22824764db459941d8519687b
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-02-27 07:54:42 +0000

    added test03

---
+++ test03/test01.js
@@ -0,0 +1,16 @@
+/*
+実行結果
+I am Alice
+*/
+
+var name = 
+
+if( name == "Micky" ) {
+    console.log("I am Micky");
+} else if( name == "Minny" ) {
+    console.log("I am Minny");
+} else if( name == "Alice" ) {
+    console.log("I am Alice");
+} else {
+    console.log("Who am I?");
+}
---
+++ test03/test05.js
@@ -0,0 +1,12 @@
+/*
+進みます
+*/
+
+
+var signal = 
+
+if( signal == '赤') {
+    console.log('止まります');
+} else {
+    console.log('進みます');
+}
